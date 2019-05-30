cf47daa10062b3923ec5ba2cfb2c92ac383440dd
kejbi <biegajski.kacper@gmail.com>
2019-05-30 23:39:23 +0200

    logo added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Home.css';
+import logo from '../Logo.jpg';
 import { loggedIn } from './security/AuthService';
+import { Container, Row, Col } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 
 class Home extends Component {
@@ -11,10 +13,15 @@ class Home extends Component {
   }
   render() {
     return (
-      <div className="home-page">
-        <h1>Welcome to Test Them All</h1>
-        <p>Test them all is a ...</p>
-      </div>
+      <Container className="home-page">
+        <div className="item1">
+          <h1>Welcome to Test Them All</h1>
+        </div>
+
+        <div className="item2">
+          <img className="logo" src={logo} alt="Logo" />
+        </div>
+      </Container>
     );
   }
 }
